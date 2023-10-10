56aef2664218eaa1a96069a3bc77d143ac7cf1ee
richardola1 <147484156+richardola1@users.noreply.github.com>
2023-10-10 09:24:55 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-// script.js
-document.addEventListener("DOMContentLoaded", function () {
-    alert("Welcome to my website!");
-});
+// // script.js
+// const starsSection = document.querySelector('.stars');
+// const body = document.body;
+
+// // Toggle animation class on click anywhere on the page
+// body.addEventListener('click', () => {
+//     starsSection.classList.toggle('animate-stars');
+// });
